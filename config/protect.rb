caaffb53ca1d3bf4493bbcb0e2dc4f05c9dfc995
Louis Mullie <louis.mullie@gmail.com>
2013-06-09 03:57:03 -0400

    Hotfixes for handlebars pipeline.

--- config/protect.rb
+++ config/protect.rb
@@ -1,5 +1,7 @@
-if $env == :production
-  use Rack::Auth::Basic, "Asocial Private Beta" do |username, password|
-    [username, password] == ['admin', 'b23~b23~']
-  end
-end
+#if $env == :production
+#  use Rack::Auth::Basic, "Asocial Private Beta" do |username, password|
+#    [username, password] == ['admin', 'b23~b23~']
+#  end
+#end
+
+# Doesn't work with compression: https://github.com/rstacruz/sinatra-assetpack/issues/68
--- public/js/asocial/config/handlebars.js
+++ public/js/asocial/config/handlebars.js
@@ -1,8 +1,10 @@
 Handlebars.registerHelper('t',
-  function(str){
-    // parse string
-    // get translation
-    var t = str;
-    return t;
+  function(path) {
+    var current = locales[locale];
+    var elements = path.split('.');
+    while (elements.length > 0 ) {
+      current = current[elements.shift()];
+    }
+    return current;
   }
 );
