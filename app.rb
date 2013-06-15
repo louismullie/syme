d78dc7a2929ec4212d939dbffd9ee5986a3b2b10
Louis Mullie <louis.mullie@gmail.com>
2013-06-14 22:36:21 -0400

    Clean up config and deploy environment.

--- app.rb
+++ app.rb
@@ -7,38 +7,31 @@ module Asocial
 
     disable :protection # Hahaha
     
-    Bundler.require :default,
-    settings.environment
+    Bundler.require :default, settings.environment
 
-    configure do
-      require_specific 'config', ['settings']
-      require_all 'config'
-    end
-
+    configure { require_all 'config' }
     helpers   { require_all 'helpers' }
-
-    helpers Fifty
 
     Dir['./models/*.rb'].each { |file| require file }
     Dir['./observers/*.rb'].each { |file| require file }
     Dir['./generators/*.rb'].each { |file| require file }
 
-    require_specific('routes', ['stream', 'catch', 'group'])
-
     require_all 'routes'
 
-    def initialize(*args, &block)
+    # to remove
+    helpers Fifty
 
+    def initialize(*args, &block)
       Fifty.context = self
       super(*args, &block)
-
     end
 
     def call(env)
       Fifty.logger = env['rack.logger']
       super(env)
     end
-
+    # to remove
+    
     Mongoid.observers = PostObserver, CommentObserver,
     LikeObserver, NotificationObserver, InviteObserver,
     UserObserver
--- base.rb
+++ base.rb
@@ -1,20 +1,11 @@
 module Asocial
 
   class Base < Sinatra::Base
-
-    def self.require_specific(dir, files=[])
-      files = files.map { |f| File.join(dir, "_#{f}") }
-      files.each { |f| eval(File.read "#{f}.rb") }
-    end
-
-    # Recursive inline eval of Ruby files.
+    
     def self.require_all(dir, opts={})
-      Dir["./#{dir}/**/*.rb"].each do |f|
-        next if f.index('deploy') || f =~ /^_/
-        eval(File.read f)
-      end
+      Dir["./#{dir}/**/*.rb"].each { |f| eval(File.read(f)) }
     end
-
+    
     # Authorizations
     set(:auth) do |*roles|
       condition { halt 403 unless session[:user_id] }
