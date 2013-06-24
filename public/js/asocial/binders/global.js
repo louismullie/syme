4fa41610c8e19a3de686455219fef7cb63d61506
Christophe Marois <christophemarois@gmail.com>
2013-06-24 02:20:23 -0400

    GOODBYE PAST FRIENDS

--- public/js/asocial/binders/global.js
+++ public/js/asocial/binders/global.js
@@ -48,41 +48,6 @@ asocial.binders.add('global', { main: function(){
     $(this).closest('form').submit();
   });
 
-  // Implement session timeouts
-  $(document).ready(function () {
-
-    var idleTime = 0;
-
-    function timerIncrement() {
-      idleTime = idleTime + 1;
-      if (idleTime > 20) { // 20 minutes
-
-        // Log out
-        $.ajax('/logout', { type: 'get'} );
-
-        // Force disconnection
-        asocial.helpers.showAlert('You have been disconnected', {
-          title: 'Disconnected',
-          submit: 'Log in',
-          closable: false,
-          onhide: function(){
-            window.location = '/login';
-          }
-        });
-
-      }
-    }
-
-    // Increment the idle time counter every minute.
-    var idleInterval = setInterval(timerIncrement, 60000); // 1 minute
-
-    // Zero the idle timer on mouse movement.
-    $(this).mousemove(function (e) { idleTime = 0; });
-
-    $(this).keypress(function (e) { idleTime = 0; });
-
-  });
-
   $(document).on('click', '#notifications-container a.notification-unread', function(e) {
 
     var id = $(this).closest('.notification').attr('id');
