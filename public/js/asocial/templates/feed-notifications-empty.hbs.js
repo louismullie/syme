1287202cd69e094940033f0de5a30432404e3bb0
Louis Mullie <louis.mullie@gmail.com>
2013-06-23 23:18:39 -0400

    Ignore compiled Handlebars templates.

--- public/js/asocial/templates/feed-notifications-empty.hbs.js
+++
@@ -1,9 +0,0 @@
-
-Handlebars.templates = Handlebars.templates || {};
-Handlebars.templates['feed-notifications-empty.hbs'] = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
-  helpers = helpers || Handlebars.helpers;
-  
-
-
-  return "<div class='notification' id='empty'>\n  <p class='empty-notification'>\n    No new notifications.\n  </p>\n</div>\n";});
-Handlebars.registerPartial('feed-notifications-empty.hbs', Handlebars.templates['feed-notifications-empty.hbs']);
