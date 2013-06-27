4d94b7927bfd63247b9ead9e28837e7b2b1f1176
Louis Mullie <louis.mullie@gmail.com>
2013-06-27 16:22:06 -0400

    Remove catch-all in Chrome environment

--- routes/index.rb
+++ routes/index.rb
@@ -1,16 +1,20 @@
-# Root
-get '/' do
-  # Render the main layout.
-  File.read('./.hbs/views/layout.hbs')
-end
+unless settings.environment = :chrome
+  
+  # Root
+  get '/' do
+    # Render the main layout.
+    File.read('./.hbs/views/layout.hbs')
+  end
 
-# Catch-all route.
-get '/*' do |route|
+  # Catch-all route.
+  get '/*' do |route|
 
-  # Pass for all AJAX requests.
-  pass if request.xhr?
+    # Pass for all AJAX requests.
+    pass if request.xhr?
 
-  # Otherwise redirect to root
-  redirect '/'
+    # Otherwise redirect to root
+    redirect '/'
 
+  end
+  
 end
