394f50746031ff4406fc4dee0c36b279020bb298
Louis Mullie <louis.mullie@gmail.com>
2013-07-04 13:53:47 -0400

    Fix socket bug

--- config/_settings.rb
+++ config/_settings.rb
@@ -16,4 +16,9 @@ if $env == :development
   set :reload_templates, true
 end
 
-set :protection, except: [:http_origin, :remote_token, :frame_options], origin_whitelist: ['chrome-extension://diifaedmnfmmdmfgbjmdnggodliffefa', 'localhost:5000', 'http://184.107.183.10', 'http://syme.io', 'syme.io', 'www.syme.io']
+set :protection,
+     except: [:http_origin, :remote_token, :frame_options],
+     origin_whitelist: [
+       'chrome-extension://diifaedmnfmmdmfgbjmdnggodliffefa',
+       'localhost:5000', 'http://184.107.183.10',
+       'http://syme.io', 'syme.io', 'www.syme.io']
