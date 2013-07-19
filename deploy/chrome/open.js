c867a824866c6e3ec0953d21c59b6037c09281ef
Louis Mullie <louis.mullie@gmail.com>
2013-07-19 17:01:58 -0400

    Ensure one tab open (fixes #160)

--- deploy/chrome/open.js
+++ deploy/chrome/open.js
@@ -1,5 +1,31 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
-  chrome.tabs.create({'url': chrome.extension.getURL('syme.html')}, function(tab) {
+  
+  var symeUrl = chrome.extension.getURL('syme.html');
+  
+  // Check whether syme is already opened.
+  chrome.tabs.query({ url: symeUrl }, function(tabs) {
     
-  });
+    // Syme is not opened in this window.
+    if (tabs.length == 0) {
+      
+      // Open Syme in a tab.
+      chrome.tabs.create(
+        {'url': symeUrl },
+        function(tab) { }
+      );
+      
+    // Syme is already open in another tab.
+    } else {
+      
+      // Close all open tabs except the first.
+      for (var i=1; i<tabs.length; i++)
+        chrome.tabs.remove(tabs[i].id);
+      
+      // Focus on the first active tab.
+      chrome.tabs.update(tabs[0].id, {active: true});
+        
+    }
+
+ });
+
 });
