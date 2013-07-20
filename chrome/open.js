bfd8d72a205d06a91d46cd5804a4433c4fe4f9c0
Louis Mullie <louis.mullie@gmail.com>
2013-07-20 12:57:05 -0400

    Delete extension

--- chrome/open.js
+++
@@ -1,8 +0,0 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
-  
-  var symeUrl = chrome.extension.getURL('syme.html');
-
-  chrome.tabs.create( {'url': symeUrl }, function(tab) { });
-
-
-});
