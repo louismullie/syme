e108cebcacb938575660e8d7800644ccaa883803
Louis Mullie <louis.mullie@gmail.com>
2013-08-28 19:14:58 -0400

    Modify mention script for Chris

--- observers/resource/notifier.rb
+++ observers/resource/notifier.rb
@@ -1,18 +1,20 @@
 module ResourceObserver::Notifier
 
   def notify_mentioned(resource)
-
-    return unless resource.mentions
     
-    user = resource.owner
-    full_names = resource.mentions
-    group = resource.parent_group
+    mentioned_users = resource.mentions
+    
+    return unless mentioned_users
+    
+    user, group = resource.owner, resource.parent_group
 
-    full_names.each do |full_name|
-
-      other_user = User.where(full_name: full_name).first
-      next unless other_user # user changed name in the meantime
-      other_user.notify_mentioned_by_in(user, resource, group)
+    mentioned_users.each do |mentioned_user_id|
+      
+      mentioned_user = group.users.where(id: mentioned_user_id).first
+      
+      next unless mentioned_user
+      
+      mentioned_user.notify_mentioned_by_in(user, resource, group)
 
     end
 
