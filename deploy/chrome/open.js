fed400f1910cb2ae09efd2d82504326c1374da98
Louis Mullie <louis.mullie@gmail.com>
2013-08-28 21:50:52 -0400

    Open registration page after installation

--- deploy/chrome/open.js
+++ deploy/chrome/open.js
@@ -1,8 +1,19 @@
-chrome.browserAction.onClicked.addListener(function(tab) {
+function openApp() {
   
   var symeUrl = chrome.extension.getURL('syme.html');
-
   chrome.tabs.create( {'url': symeUrl }, function(tab) { });
+  
+}
 
+chrome.storage.local.get('hasBeenOpened', function (setting) {
+  
+  if (!setting.hasBeenOpened) {
+    
+    chrome.storage.local.set({ hasBeenOpened:  true });
+    openApp();
 
+  }
+  
 });
+
+chrome.browserAction.onClicked.addListener(openApp);
