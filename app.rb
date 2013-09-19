ca7d84fa1178152ecc8db30617697372a6b81991
Christophe Marois <christophemarois@gmail.com>
2013-09-19 14:58:44 -0400

    Updated version to 0.1.7

--- app.rb
+++ app.rb
@@ -4,7 +4,7 @@ module Syme
 
   class Application < Base
 
-    VERSION = '0.1.6'
+    VERSION = '0.1.7'
 
     Bundler.require :default, settings.environment
 
--- app/js/app/headers.js
+++ app/js/app/headers.js
@@ -5,5 +5,5 @@ Syme = {
     updatedPosts: {},
     updatedComments: {}
   },
-  version: '0.1.6'
+  version: '0.1.7'
 };
