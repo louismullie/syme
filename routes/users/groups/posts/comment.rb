40aaa761bf365f186c3a17b789fc11ca53762c0f
Louis Mullie <louis.mullie@gmail.com>
2013-09-25 14:45:13 -0400

    Testing

--- routes/users/groups/posts/comment.rb
+++ routes/users/groups/posts/comment.rb
@@ -36,24 +36,33 @@ end
 # Create comment
 put '/users/:user_id/groups/:group_id/posts/:post_id/comments/:comment_id', auth: [] do |user_id, group_id, post_id, comment_id|
 
+  warn "1"
+  
   encrypted = params.dup
   params = decrypt_params(encrypted)
   
+  warn "2"
+  
   error 400, 'missing_params' if !params[:content]
+  
   
   message = JSON.parse(Base64.strict_decode64(params[:content]))
   
-  @group = begin
+  group = begin
     @user.groups.find(group_id)
   rescue Mongoid::Errors::DocumentNotFound
     error 404, 'group_not_found'
   end
   
+  warn "3"
+  
   post = begin
-    @group.complete_posts.find(post_id)
+    group.complete_posts.find(post_id)
   rescue Mongoid::Errors::DocumentNotFound
     error 404, 'post_not_found'
   end
+  
+  warn "4"
   
   comment = begin
     post.comments.find(comment_id)
@@ -61,10 +70,14 @@ put '/users/:user_id/groups/:group_id/posts/:post_id/comments/:comment_id', auth
     error 404, 'comment_not_found'
   end
   
+  warn "5"
+  
   if (user_id  != @user.id.to_s)   ||
      (@user.id.to_s != comment.owner_id.to_s)
     error 403, 'access_unauthorized'
   end
+  
+  warn "6"
   
   comment.content = message['message']
   comment.keys = message['keys']
