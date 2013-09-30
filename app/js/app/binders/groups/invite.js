a47494c63f45d661d905b0792b2e13e1d46083e9
Christophe Marois <christophemarois@gmail.com>
2013-09-29 20:39:15 -0400

    Fingerprints in group cards

--- app/js/app/binders/groups/invite.js
+++ app/js/app/binders/groups/invite.js
@@ -5,24 +5,47 @@ Syme.Binders.add('groups', { invite: function() {
     e.preventDefault();
     Invitation.acceptInvitationRequest($(this));
   });
-  
-  // Accept an invitation to join a group.
-  $('#main').on('click', '.invite-link[data-invite-state="2"]', function (e) {
 
-    var groupId = $(this).data('invite-group_id'),
-        inviterId = $(this).data('invite-inviter_id');
-      
-    Syme.Crypto.getKeyFingerprint(groupId, inviterId, 'invitee', null,
-    
-      function (fingerprint) {
+  $('#main').on('click', 'a.fingerprint-link[data-toggler="true"]', function(e){
 
-        prompt('Your key fingerprint is: ', fingerprint.inviteeFingerprint);
-        prompt('Inviter fingerprint is: ', fingerprint.inviterFingerprint);
+    var $fingerprintBox = $(this).closest('.group-card-content').find('.fingerprint-box'),
+        show            = $fingerprintBox.hasClass('hidden');
 
-      }
-    
-    );
-    
+    $(this)[show ? 'addClass' : 'removeClass']('expanded');
+    $fingerprintBox[ show ? 'removeClass' : 'addClass' ]('hidden');
+
+  });
+
+  // View fingerprints for pending invitation
+  $('#main').on('click', 'a.fingerprint-link[data-toggler!="true"]', function (e) {
+
+    var $this           = $(this),
+        $fingerprintBox = $this.closest('.group-card-content').find('.fingerprint-box'),
+        $inviteLink     = $this.closest('.group-card').find('a.invite-link');
+
+    var groupId   = $inviteLink.data('invite-group_id'),
+        inviterId = $inviteLink.data('invite-inviter_id');
+
+    var trimFingerprint = function(fullFingerprint) {
+      return fullFingerprint.replace(/:/g, '').substr(0, 6);
+    }
+
+    Syme.Crypto.getKeyFingerprint(groupId, inviterId, 'invitee', null, function (fingerprint) {
+
+      // Insert asynchronously gotten fingerprints
+      $('.you', $fingerprintBox)
+        .text(trimFingerprint(fingerprint.inviteeFingerprint))
+        .attr('title', fingerprint.inviteeFingerprint);
+
+      $('.inviter', $fingerprintBox)
+        .text(trimFingerprint(fingerprint.inviterFingerprint))
+        .attr('title', fingerprint.inviterFingerprint);
+
+      // Make link a toggler and toggle it
+      $this.attr('data-toggler', true).trigger('click');
+
+    });
+
   });
 
 } }); // Syme.Binders.add();
