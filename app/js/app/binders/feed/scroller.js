c3c98729ef21a3a07753509f21b2dda853edcbe8
Christophe Marois <christophemarois@gmail.com>
2013-10-17 12:12:21 -0400

    Complete blind-rewrite of scroller

--- app/js/app/binders/feed/scroller.js
+++ app/js/app/binders/feed/scroller.js
@@ -1,122 +1,98 @@
 Syme.Binders.add('feed', { scroller: function(){
 
-  // Infinite scroller
-  $('#feed').data('pagesloaded', 1);
+  $('#feed').prop('scroller', {
 
-  $(window).data('infinite-scroll-done', false)
-           .data('infinite-scroll-async', false)
-           .data('infinite-scroll-manual', false);
+    /* Configuration */
 
-  $(window).on('scroll infinitescroll.trigger', function(){
+    offset: 50,
 
-    if( !$(window).data('infinite-scroll-started') ||
-        $(window).data('infinite-scroll-done') ||
-        $(window).data('infinite-scroll-async') ||
-        $(window).data('infinite-scroll-manual') ) return;
+    /* Elements */
 
-    if($(window).scrollTop() >= $(document).height() - $(window).height() - 50){
+    $window:    $(window),
+    $document:  $(document),
+    $feed:      $('#feed'),
+    $loadMore:  $('#load-more'),
 
-      // Lock semaphore before AJAX request
-      $(window).data('infinite-scroll-async', true);
+    /* Methods */
 
-      // Creates an array containing all showed posts
-      var showed_posts_id = Array();
+    handler: (function(self){
 
-      $.each($('.post'), function(index, value){
-        showed_posts_id.push($(this).attr('id'));
+      var _this = self;
+
+      return _this.$window.on('scroll feedScroller.trigger', function(){
+
+        var withinLimits = ( _this.$window.scrollTop() >=
+          _this.$document.height() - _this.$window.height() - this.offset );
+
+        if ( withinLimits && _this.loadedPages && !_this.paused )
+          _this.trigger();
+
       });
 
-      // Prevent triggering scroller when no posts shown
-      if (showed_posts_id.length == 0) return;
+    })(this),
 
-      // Increment feed's state
-      var toload = $('#feed').data('pagesloaded') + 1;
+    trigger: function(){
 
-      // Build post request
-      var request = {
-        // Post a list of already showed posts to prevent duplication
-        'ignore': showed_posts_id,
-        'page': toload
-      };
+      var _this = this;
 
-      // Add optional year and month to request
-      if($('#feed').data('year')) request['year'] = $('#feed').data('year');
-      if($('#feed').data('month')) request['month'] = $('#feed').data('month');
-
-      // Spinner and loadmore
+      _this.paused = true;
+      _this.$loadMore.show();
       NProgress.showSpinner();
-      $('#load-more').show();
 
-      var groupId = Syme.CurrentSession.getGroupId();
-      var url = SERVER_URL + '/' + groupId + '/page';
+      var url = SERVER_URL + '/' + Syme.CurrentSession.getGroupId() + '/page';
 
-      $.post(url, request, function(data){
+      $.post( url, { page: ( _this.loadedPages + 1 ) }, function(data){
 
         NProgress.hideSpinner();
 
-        // Deactivate and return if there are no
-        // more posts to load
-        if ( _.isEmpty(data) ) {
+        // Detach if there are no more pages to load
+        if ( _.isEmpty(data) ) _this.detach();
 
-          $(window).data('infinite-scroll-done', true);
-          $('#load-more').hide();
+        var doneDecryptingCb = function(){
 
-          return;
+          _this.loadedPages++;
+          _this.$loadMore.hide();
+          _this.paused = false;
 
-        }
+        };
 
-        // Deactivate if this is the last page
-        $(window).data( 'infinite-scroll-done', data.last_page ? true : false );
-
-        // Generate templates for each post
-        var html = $();
-
-        _.each(data.posts, function(post){
-
-          // Add generated post to the $html jQuery collection
-          html = html.add(
-            $( Syme.Template.render('feed-post', post) )
-          );
-
-        });
-
-        // Update counter
-        $('#feed').data('pagesloaded', toload);
-
-        // Append generated templates
-        $('#feed').append( html );
-
-        // Decrypt new content
-        Syme.Decryptor.decryptPostsAndCommentsInContainer($('#feed'), function(){
-
-          // Show or hide load-more
-          $('#load-more').hide();
-
-        });
-
-      }).complete(function(){
-
-        // Release lock after AJAX request
-        $(window).data('infinite-scroll-async', false);
+        _this.render(data, doneDecryptingCb);
 
       });
+
+    },
+
+    render: function(data, doneDecryptingCb) {
+
+      var _this = this;
+
+      var $collection = $();
+      _.each(data.posts, function(post){
+        $collection = $collection.add(
+          $( Syme.Template.render('feed-post', post) );
+        );
+      });
+
+      _this.$feed.append($collection);
+      Syme.Decryptor.decryptPostsAndComments($collection, doneDecryptingCb);
+
+    },
+
+    detach: function(){
+
+      var _this = this;
+
+      _this.$window.off(this.handler);
+      _this.$loadMore.remove();
+
     }
 
   });
 
   // Load more button
   $('#main').on('click', '#load-more a', function(e){
-
-    e.preventDefault();
-
-    // Hide load-more container
     $(this).parent().fadeOut('fast');
-
-    // Unlock infinite scrolling and load page
-    $(window)
-      .data('infinite-scroll-manual', false)
-      .trigger('infinitescroll.trigger');
-
+    $(window).trigger('infinitescroll.trigger');
   });
 
 } }); // Syme.Binders.add();
