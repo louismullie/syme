660ec547340849a8950da6141ee2b55cfc86c218
Louis Mullie <louis.mullie@gmail.com>
2013-11-17 02:00:13 -0500

    Fix

--- generators/avatar.rb
+++ generators/avatar.rb
@@ -8,12 +8,18 @@ class AvatarGenerator
 
       current_key = avatar.key_for_user(current_user)
 
+      owner_id = begin
+        avatar.owner.id.to_s
+      rescue
+        ''
+      end
+      
       content = Base64.strict_encode64({
         message: avatar.key,
         keys: {
           current_user.id.to_s => current_key
         },
-        senderId: avatar.owner.id.to_s
+        senderId: owner_id
       }.to_json)
 
       {
