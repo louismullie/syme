b4fd275d59b9ef4afcc68e52a4c52822bcd7c6fb
Louis Mullie <louis.mullie@gmail.com>
2013-11-19 21:01:35 -0500

    Revert

--- app/js/launch.js
+++
@@ -1,1 +0,0 @@
-window.location = 'chrome-extension://kebgjahkgfpaeidbimpiefobehkjmani/syme.html';
--- routes/app.rb
+++ routes/app.rb
@@ -1,21 +1,3 @@
 get '/launch' do
-
-  content_type 'text/html'
-  
-<<-EOF
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-</head>
-
-<body>
-Redirecting you to the Syme application...
-<a href="chrome-extension://kebgjahkgfpaeidbimpiefobehkjmani/syme.html"></a>
-</body>
-
-</html>
-
-EOF
+  redirect 'chrome-extension://kebgjahkgfpaeidbimpiefobehkjmani/syme.html'
 end
