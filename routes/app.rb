9f5b58ab115ae4e7450910caacefc722aff97d72
Louis Mullie <louis.mullie@gmail.com>
2013-11-19 20:42:04 -0500

    Testing new redirect

--- routes/app.rb
+++ routes/app.rb
@@ -1,3 +1,22 @@
 get '/launch' do
-  redirect 'chrome-extension://kebgjahkgfpaeidbimpiefobehkjmani/syme.html'
+<<-EOF
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+<script type="text/javascript">
+window.location = 'chrome-extension://kebgjahkgfpaeidbimpiefobehkjmani/syme.html';
+</script>
+
+</head>
+
+<body>
+Redirecting you to the Syme application...
+</body>
+
+</html>
+
+EOF
 end
