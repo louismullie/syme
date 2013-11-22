00608cb8191de56ed718c383fbc550717ecbd5da
Louis Mullie <louis.mullie@gmail.com>
2013-11-21 22:03:07 -0500

    Fix

--- app/js/app/launch.js
+++ app/js/app/launch.js
@@ -5,4 +5,4 @@ function(response) {
   if (!response.success) throw 'Could not open app.';
 });
 
-//setTimeout(function(){var ww = window.open(window.location, '_self'); ww.close(); }, 500);
+setTimeout(function(){var ww = window.open(window.location, '_self'); ww.close(); }, 500);
