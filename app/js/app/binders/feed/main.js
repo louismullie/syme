b52890754df848314ef2e9f4213c820f31312ba4
Christophe Marois <christophemarois@gmail.com>
2013-11-22 17:26:29 -0500

    Fix bug where infinite scroller would trigger in single-post view

--- app/js/app/binders/feed/main.js
+++ app/js/app/binders/feed/main.js
@@ -3,13 +3,21 @@ Syme.Binders.add('feed', { main: function(){
   // Initial media decryption
   $('.user-avatar, .encrypted-image').trigger('decrypt');
 
-  console.time('Initial decryption/formatting');
+  // Start decryption timer
+  if (DEVELOPMENT)
+    console.time('Initial decryption/formatting');
 
-  Syme.Decryptor.decryptPostsAndCommentsInContainer($('#feed'), function(){
+  var feed = $('#feed');
 
-    console.timeEnd('Initial decryption/formatting');
+  Syme.Decryptor.decryptPostsAndCommentsInContainer(feed, function(){
 
-    $('#feed').prop('scroller', new Syme.FeedScroller);
+    // End decryption timer
+    if (DEVELOPMENT)
+      console.timeEnd('Initial decryption/formatting');
+
+    // Initiate feed scroller if we aren't in single post view
+    if ( !feed.is('[data-single-post="true"]') )
+      feed.prop('scroller', new Syme.FeedScroller);
 
   });
 
